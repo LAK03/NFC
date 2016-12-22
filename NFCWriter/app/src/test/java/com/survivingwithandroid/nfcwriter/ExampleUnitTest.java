be99d31871bed52c652599c2c467f40b88b0943b
CocosoftAndroid <cocosoftinc@gmail.com>
2016-12-21 22:03:26 -0800

    test commit with comment

--- NFCWriter/app/src/test/java/com/survivingwithandroid/nfcwriter/ExampleUnitTest.java
+++ NFCWriter/app/src/test/java/com/survivingwithandroid/nfcwriter/ExampleUnitTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
+		// test comment
         assertEquals(4, 2 + 2);
     }
 }
